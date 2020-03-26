e2976fb7bb5680ad5078d4fa236b30a8a10c84f0
Lukas Brandt <lubrandt@innosystec.de>
2020-03-26 09:11:37 +0100

    disable unused implementations for now (comment out)

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,22 +35,22 @@ kotlin {
         }
         val commonTest by getting {
             dependencies {
-                implementation(kotlin("test-common"))
-                implementation(kotlin("test-annotations-common"))
+//                implementation(kotlin("test-common"))
+//                implementation(kotlin("test-annotations-common"))
             }
         }
         val jvmMain by getting {
             dependencies {
                 implementation(kotlin("stdlib-jdk8"))
                 implementation("io.ktor:ktor-server-netty:$ktorVersion")
-                implementation("io.ktor:ktor-html-builder:$ktorVersion")
+//                implementation("io.ktor:ktor-html-builder:$ktorVersion")
                 implementation("ch.qos.logback:logback-classic:$logbackVersion")
             }
         }
         val jvmTest by getting {
             dependencies {
-                implementation(kotlin("test"))
-                implementation(kotlin("test-junit"))
+//                implementation(kotlin("test"))
+//                implementation(kotlin("test-junit"))
             }
         }
         val jsMain by getting {
@@ -58,25 +58,25 @@ kotlin {
                 implementation(kotlin("stdlib-js"))
 
                 //React, React DOM + Wrappers (chapter 3)
-                implementation("org.jetbrains:kotlin-react:16.9.0-pre.89-kotlin-1.3.60")
-                implementation("org.jetbrains:kotlin-react-dom:16.9.0-pre.89-kotlin-1.3.60")
-                implementation(npm("react", "16.12.0"))
-                implementation(npm("react-dom", "16.12.0"))
-
-                //Kotlin Styled (chapter 3)
-                implementation("org.jetbrains:kotlin-styled:1.0.0-pre.90-kotlin-1.3.61")
-                implementation(npm("styled-components", "5.0.1"))
-                implementation(npm("react-is", "16.12.0"))
-                implementation(npm("inline-style-prefixer", "5.1.2"))
-
-                //Video Player (chapter 7)
-                implementation(npm("react-player", "1.15.2"))
-
-                //Share Buttons (chapter 7)
-                implementation(npm("react-share", "4.0.1"))
-
-                //Coroutines (chapter 8)
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-common:1.3.3")
+//                implementation("org.jetbrains:kotlin-react:16.9.0-pre.89-kotlin-1.3.60")
+//                implementation("org.jetbrains:kotlin-react-dom:16.9.0-pre.89-kotlin-1.3.60")
+//                implementation(npm("react", "16.12.0"))
+//                implementation(npm("react-dom", "16.12.0"))
+//
+//                //Kotlin Styled (chapter 3)
+//                implementation("org.jetbrains:kotlin-styled:1.0.0-pre.90-kotlin-1.3.61")
+//                implementation(npm("styled-components", "5.0.1"))
+//                implementation(npm("react-is", "16.12.0"))
+//                implementation(npm("inline-style-prefixer", "5.1.2"))
+//
+//                //Video Player (chapter 7)
+//                implementation(npm("react-player", "1.15.2"))
+//
+//                //Share Buttons (chapter 7)
+//                implementation(npm("react-share", "4.0.1"))
+//
+//                //Coroutines (chapter 8)
+//                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-common:1.3.3")
             }
         }
         val jsTest by getting {
